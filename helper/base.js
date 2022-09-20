1af3ac39ee755730e2989ebdb403f42a566bd5ef
manish272 <51703822+manish272@users.noreply.github.com>
2022-09-20 12:16:52 +0530

    Update base.js

--- helper/base.js
+++ helper/base.js
@@ -11,7 +11,7 @@ module.exports = {
       .join("+")}`,
   playlistFromID: (id) =>
     `https://www.jiosaavn.com/api.php?__call=webapi.get&type=playlist&p=1&n=50&includeMetaTags=0&ctx=web6dot0&api_version=4&_format=json&_marker=0&token=${id}`,
-  songFromID: (id) =>
+  songFromSTRING: (id) =>
     `https://www.jiosaavn.com/api.php?__call=song.getDetails&cc=in&_marker=0%3F_marker%3D0&_format=json&pids=${id}`,
   songsearchFromSTRING: (query) =>
     `https://www.jiosaavn.com/api.php?p=1&_format=json&_marker=0&api_version=4&ctx=wap6dot0&n=10&__call=search.getResults&q=${query
